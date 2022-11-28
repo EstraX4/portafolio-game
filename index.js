13e1ac2ce2906cf88ac331f6d54771b9a77521ac
Juan David Estrada Carvajal <Estraxclips@gmail.com>
2022-11-28 18:16:27 -0500

    update

--- index.js
+++ index.js
@@ -4,23 +4,7 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16;
 canvas.height = 64 * 9;
 
-class Player {
-    constructor() {
-        this.position = {
-            x: 100,
-            y: 100
-        }
-
-        this.width = 100
-        this.height = 100
-    }
-    drawn() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-}
-
-const player = new Player()
+const player = new Player();
 
 //let bottom = y + 100
 
@@ -29,12 +13,12 @@ function animate() {
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  player.drawn()
-
-//   if (bottom < canvas.height) {
-//     y++
-//     bottom = y + 100
-//   }
+  player.drawn();
+  player.update();
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  console.log(event)
+});
